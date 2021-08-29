d8506b0f6ebb3b44bd977704b6f9151b0fe011bc
thamarais16 <thamarais16@gmail.com>
2021-08-29 19:08:43 +0530

    2 files modified, 4 added

--- src/app/app-http-interceptor.service.ts
+++ src/app/app-http-interceptor.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpInterceptor, HttpEvent } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpRequest, HttpHandler, HttpInterceptor, HttpEvent, HttpResponse } from "@angular/common/http";
+import { Observable} from "rxjs";
 
 @Injectable()
 export class AppHttpInterceptorService implements HttpInterceptor {
 
   constructor() { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>>{
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
     console.log(req);
     
+    // req = req.clone({headers: req.headers.append('Content-Type', 'application/json')});
+    // req = req.clone({headers: req.headers.append('Access-Control-Allow-Origin', '*')});
+    // req = req.clone({setHeaders: {'Content-Type':'application/x-www-form-urlencoded'}});
+
+
     return next.handle(req);
-  }
+  };
 
 }
